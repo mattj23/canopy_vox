55cf949cf47342d8bc310407ebbcd9895df16694
Matt Jarvis <mattj23@gmail.com>
2017-04-09 07:10:34 -0400

    plot summary update

--- tools/plot_summary.py
+++ tools/plot_summary.py
@@ -4,6 +4,7 @@
 Plot the summary file in the simplest way possible
 """
 
+import os
 import numpy
 import argparse
 import matplotlib.pyplot as plt
@@ -14,14 +15,32 @@ except:
     from tools.sparsevox import SparseVoxels
 
 
+def load_plot_file(file_name):
+    points = []
+    with open(file_name, "r") as handle:
+        for line in handle:
+            try:
+                x, y, *_ = line.split()
+                x = float(x)
+                y = float(y)
+                points.append([x, y])
+            except:
+                continue
+    return zip(*points)
+
 def main():
     parser = argparse.ArgumentParser(description="Plots the top-down view of the voxel densities for a summary file")
     parser.add_argument("file", help="The .sparsevox file to process")
     parser.add_argument("output", help="The name of the png to output")
+    parser.add_argument("--plot-green", action="store", dest="plot_green", help="A file to plot green circles at")
     args = parser.parse_args()
 
+    # Load the voxels from the file
+    print("Loading voxels from {}".format(args.file))
     voxels = SparseVoxels(file=args.file)
+    print("Done")
 
+    # Determine the entire collection of all i and j values
     i_values = set()
     j_values = set()
 
@@ -29,18 +48,34 @@ def main():
         i_values.add(voxel.i)
         j_values.add(voxel.j)
 
-    i0 = min(i_values)
-    j0 = min(j_values)
-    imax = max(i_values)
-    jmax = max(j_values)
+    # The maximum and minimum values of i and j will determine what the  bounding space of the voxel field is,
+    # as well as the offset values i_0 and j_0 which will be necessary to transform from voxel space into image
+    # space (by taking a coordinate in voxel space and subtracting (i_0, j_0))
+    i_0 = min(i_values)
+    j_0 = min(j_values)
+    i_max = max(i_values)
+    j_max = max(j_values)
 
-    print("i0, j0 = {}, {}".format(i0, j0))
-    image = numpy.zeros(shape=(imax-i0+1, jmax-j0+1))
+    print("Voxel cloud summary")
+    print("i_0, i_max = {}, {} ({:.2f}m to {:.2f}m)".format(i_0, i_max, i_0 * voxels.spacing, i_max * voxels.spacing))
+    print("j_0, j_max = {}, {} ({:.2f}m to {:.2f}m)".format(j_0, j_max, j_0 * voxels.spacing, j_max * voxels.spacing))
+
+    image = numpy.zeros(shape=(i_max-i_0+1, j_max-j_0+1))
 
     for voxel in voxels:
-        image[voxel.i-i0, voxel.j-j0] += voxel.v
+        image[voxel.i-i_0, voxel.j-j_0] += voxel.v
 
-    plt.imshow(image, vmin=0, vmax=image.mean() * 2, cmap="gray")
+    intensity_max = (image.max() * 0.5)
+    plt.imshow(image, vmin=0, vmax=intensity_max, cmap="gray")
+
+    # Plot any files
+    if os.path.exists(args.plot_green):
+        green_x, green_y = load_plot_file(args.plot_green)
+        g_j = [(x / voxels.spacing) + j_0 for x in green_x]
+        g_i = [(y / voxels.spacing) + i_0 for y in green_y]
+        plt.scatter(x=g_i, y=g_j, c="g", s=40)
+
+    plt.show()
     plt.savefig(args.output)
 
 if __name__ == '__main__':
